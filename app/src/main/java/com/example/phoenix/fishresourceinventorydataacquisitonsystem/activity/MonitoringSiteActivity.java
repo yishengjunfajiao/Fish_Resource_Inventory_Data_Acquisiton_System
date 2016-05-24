3e5f97d26f32bc67750779c4adcb5fd4df386281
cjx-great <1623797535@qq.com>
2016-05-24 21:52:31 +0800

    小改

--- app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/MonitoringSiteActivity.java
+++ app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/MonitoringSiteActivity.java
@@ -1,8 +1,11 @@
 package com.example.phoenix.fishresourceinventorydataacquisitonsystem.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.phoenix.fishresourceinventorydataacquisitonsystem.R;
 
@@ -19,6 +22,16 @@ public class MonitoringSiteActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        //测试用
+        TextView title = (TextView) findViewById(R.id.mon_site_title);
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MonitoringSiteActivity.this, FractureSurfaceActivity.class));
+            }
+        });
+
     }
+
 
 }
--- app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/WaterCourseActivity.java
+++ app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/WaterCourseActivity.java
@@ -1,11 +1,11 @@
 package com.example.phoenix.fishresourceinventorydataacquisitonsystem.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.phoenix.fishresourceinventorydataacquisitonsystem.R;
 
@@ -19,14 +19,15 @@ public class WaterCourseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_course);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //测试用
+        TextView title = (TextView) findViewById(R.id.water_title);
+        title.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(WaterCourseActivity.this,CatchActivity.class));
             }
         });
     }
