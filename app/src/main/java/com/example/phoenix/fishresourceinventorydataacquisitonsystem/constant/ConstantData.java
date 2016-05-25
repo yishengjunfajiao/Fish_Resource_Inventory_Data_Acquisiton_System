7f24f41813d70e2fe848813fe320fef86c42a4ba
cjx-great <1623797535@qq.com>
2016-05-25 12:22:53 +0800

    小改4

---
+++ app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/constant/ConstantData.java
@@ -0,0 +1,10 @@
+package com.example.phoenix.fishresourceinventorydataacquisitonsystem.constant;
+
+/**
+ * @author CJX
+ *         Created on 2016/5/25.
+ * 存放静态数据
+ */
+public class ConstantData {
+
+}
