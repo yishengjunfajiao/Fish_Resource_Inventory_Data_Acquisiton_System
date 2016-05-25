5d3ec5f9baa44beb8e210c797090f860fa229650
cjx-great <1623797535@qq.com>
2016-05-25 21:35:21 +0800

    小改5

--- app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/MonitoringSiteActivity.java
+++ app/src/main/java/com/example/phoenix/fishresourceinventorydataacquisitonsystem/activity/MonitoringSiteActivity.java
@@ -4,15 +4,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import com.example.phoenix.fishresourceinventorydataacquisitonsystem.R;
+import com.example.phoenix.fishresourceinventorydataacquisitonsystem.constant.ConstantData;
 
 /**
  * 维护 监测点 界面
  * */
-public class MonitoringSiteActivity extends AppCompatActivity {
+public class MonitoringSiteActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
+        View.OnClickListener{
+
+    //新增断面
+    private GridLayout addFraSur = null;
+    //监测单位
+    private EditText detectionUnit = null;
+    //监测人员
+    private EditText monitors = null;
+    //监测日期
+    private EditText monitoringDate = null;
+    //省
+    private Spinner province = null;
+    //市
+    private Spinner city = null;
+    //镇/村
+    private EditText village = null;
+    //水域
+    private EditText waterArea = null;
+    //开始时间
+    private EditText startTime = null;
+    //结束时间
+    private EditText endTime = null;
+    //天气
+    private EditText weather = null;
+    //气温
+    private EditText temperature = null;
+    //起点经度
+    private EditText startLongitude = null;
+    //起点纬度
+    private EditText startLatitude = null;
+    //终点经度
+    private EditText endLongitude = null;
+    //终点纬度
+    private EditText endLatitude = null;
+    //添加照片
+    private GridLayout addPicture = null;
+
+    private ArrayAdapter<String> provinceAdapter = null;
+    private ArrayAdapter<String> cityAdapter = null;
+
+    private View addSurfaceView = null;
+    private View addPictureView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +73,57 @@ public class MonitoringSiteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        init();
+    }
+
+    private void init(){
+        int size = getWindowManager().getDefaultDisplay().getWidth();
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(size/5,size/5);
+
+        addFraSur = (GridLayout) findViewById(R.id.mon_site_add_fra_sur);
+        addSurfaceView = LayoutInflater.from(MonitoringSiteActivity.this)
+                .inflate(R.layout.monitor_site_grid_add_fra_surface,null);
+        addSurfaceView.setLayoutParams(params);
+        ImageView add1 = (ImageView) addSurfaceView.findViewById(R.id.image_add_surf);
+        add1.setOnClickListener(this);
+        addFraSur.addView(addSurfaceView);
+
+        detectionUnit = (EditText) findViewById(R.id.detection_unit);
+        monitors = (EditText) findViewById(R.id.monitors);
+        monitoringDate = (EditText) findViewById(R.id.monitoring_date);
+
+        province = (Spinner) findViewById(R.id.province);
+        provinceAdapter = new ArrayAdapter<>(MonitoringSiteActivity.this,
+                android.R.layout.simple_spinner_item, ConstantData.PROVINCE);
+        province.setAdapter(provinceAdapter);
+        province.setSelection(16);
+        province.setOnItemSelectedListener(this);
+
+        city = (Spinner) findViewById(R.id.city);
+        cityAdapter = new ArrayAdapter<>(MonitoringSiteActivity.this,
+                android.R.layout.simple_spinner_item,ConstantData.CITY[16]);
+        city.setAdapter(cityAdapter);
+        city.setOnItemSelectedListener(this);
+
+        village = (EditText) findViewById(R.id.village);
+        waterArea = (EditText) findViewById(R.id.water_area);
+        startTime = (EditText) findViewById(R.id.start_time);
+        endTime = (EditText) findViewById(R.id.end_time);
+        weather = (EditText) findViewById(R.id.weather);
+        temperature = (EditText) findViewById(R.id.temperature);
+        startLongitude = (EditText) findViewById(R.id.start_longitude);
+        startLatitude = (EditText) findViewById(R.id.start_latitude);
+        endLongitude = (EditText) findViewById(R.id.end_longitude);
+        endLatitude = (EditText) findViewById(R.id.end_latitude);
+
+        addPicture = (GridLayout) findViewById(R.id.mon_site_add_pic);
+        addPictureView = LayoutInflater.from(MonitoringSiteActivity.this)
+                .inflate(R.layout.monitor_site_grid_add_pic,null);
+        addPictureView.setLayoutParams(params);
+        ImageView add2 = (ImageView) addPictureView.findViewById(R.id.image_add_pic);
+        add2.setOnClickListener(this);
+        addPicture.addView(addPictureView);
 
     }
 
@@ -39,11 +142,38 @@ public class MonitoringSiteActivity extends AppCompatActivity {
         }else if (itemId == R.id.data_submit){
 
             return true;
-        }else if (itemId == R.id.turn){     //测试用，到时候删除
-            startActivity(new Intent(MonitoringSiteActivity.this, FractureSurfaceActivity.class));
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+            case R.id.province:
+                cityAdapter = new ArrayAdapter<>(MonitoringSiteActivity.this,
+                        android.R.layout.simple_spinner_item,ConstantData.CITY[position]);
+                city.setAdapter(cityAdapter);
+                break;
+            case R.id.city:
+
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.image_add_surf:
+                startActivity(new Intent(MonitoringSiteActivity.this, FractureSurfaceActivity.class));
+                break;
+            case R.id.image_add_pic:
+
+                break;
+        }
+    }
 }
